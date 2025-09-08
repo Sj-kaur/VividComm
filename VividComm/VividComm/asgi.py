9e3bca84ea036707ad3d1647b238644095b4827d
Simar <stardust.hcl@gmail.com>
2025-09-08 16:31:23 +0530

    Day 8: Configured Django Channels, ASGI, and created initial routing setup.

--- VividComm/VividComm/asgi.py
+++ VividComm/VividComm/asgi.py
@@ -1,16 +1,21 @@
-"""
-ASGI config for VividComm project.
-
-It exposes the ASGI callable as a module-level variable named ``application``.
-
-For more information on this file, see
-https://docs.djangoproject.com/en/5.2/howto/deployment/asgi/
-"""
-
+# VividComm/asgi.py
 import os
+import django
 
+from channels.auth import AuthMiddlewareStack
+from channels.routing import ProtocolTypeRouter, URLRouter
 from django.core.asgi import get_asgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'VividComm.settings')
+from . import routing
 
-application = get_asgi_application()
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'VividComm.settings')
+django.setup()
+
+application = ProtocolTypeRouter({
+  "http": get_asgi_application(),
+  "websocket": AuthMiddlewareStack(
+      URLRouter(
+          routing.websocket_urlpatterns
+      )
+  ),
+})
---
+++ VividComm/VividComm/routing.py
@@ -0,0 +1,11 @@
+# VividComm/routing.py
+from django.urls import re_path
+from channels.routing import ProtocolTypeRouter, URLRouter
+
+websocket_urlpatterns = [
+    # re_path(r'ws/chat/(?P<room_name>\w+)/$', consumers.ChatConsumer.as_asgi()),
+]
+
+application = ProtocolTypeRouter({
+    'websocket': URLRouter(websocket_urlpatterns),
+})
